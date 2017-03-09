ff63eab501a01a8a52159d37537aee3024b532be
Danil Migachev <dowhileman@gmail.com>
2017-03-10 02:49:45 +0300

    jobs-055: add  Drawer

--- app/containers/App/index.tsx
+++ app/containers/App/index.tsx
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
+import autobind from 'autobind-decorator';
 import { Snackbar } from 'react-native-material-design';
 import { connect } from 'react-redux';
+import { DrawerLayoutAndroid } from 'react-native';
+
+import { pushScene, popScene } from '../SceneNavigator/actions'
 import selectors from './selectors';
-
+import IScene from "types/scene.interface";
 import SceneNavigator from '../SceneNavigator';
+import Navigation from './components/Navigation';
 
-class App extends Component<null, null> {
+interface Props{
+  navigationState: any;
+  pushScene();
+  popScene();
+}
+
+@autobind
+class App extends Component<Props, null> {
+  refs: {
+    drawer: any
+  };
+  openDrawer(){
+    this.refs.drawer.openDrawer()
+  };
+  renderNavigationView(){
+  }
   render() {
     return (
-      <SceneNavigator/>
+      <DrawerLayoutAndroid drawerWidth={300}
+                           ref="drawer"
+                           drawerPosition={DrawerLayoutAndroid.positions.Left}
+                           renderNavigationView={this.renderNavigationView}
+      >
+        <SceneNavigator openDrawer={this.openDrawer}/>
+      </DrawerLayoutAndroid>
     );
   }
 }
 
 const mapStateToProps = state => selectors(state);
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch => ({
+  popScene: () => dispatch(popScene()),
+  pushScene: (scene: IScene) => dispatch(pushScene(scene))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- app/containers/App/selectors.ts
+++ app/containers/App/selectors.ts
@@ -1,8 +1,11 @@
 import { createSelector } from 'reselect';
 
 const selectSubstate = state => state.global.app;
+const selectNavigatorState = state => state.global.navigator.navigationState;
+
 
 export default createSelector(
   selectSubstate,
-  (substate) => substate
+  selectNavigatorState,
+  (substate, navigationState) => ({...substate, navigationState})
 )
