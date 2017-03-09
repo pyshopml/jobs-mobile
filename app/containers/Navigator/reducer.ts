a4159b63c432f9e690a38f968809f8055657cd21
Danil Migachev <dowhileman@gmail.com>
2017-03-09 22:59:41 +0300

    jobs-053: do some refactor

--- app/containers/Navigator/reducer.ts
+++ app/containers/Navigator/reducer.ts
@@ -3,38 +3,50 @@ import {
   POP_SCENE
 } from './constants';
 import scenes from 'scenes';
+import IScene from 'types/scene.interface'
 import {NavigationExperimental} from 'react-native';
-const {StateUtils: NavigationStateUtils,} = NavigationExperimental;
+const {StateUtils: NavigationStateUtils} = NavigationExperimental;
 
-const initialModel = {
-  navigationState: {
-    index: 0,
-    routes: [{}]
+function pushScene(navigationState, scene){
+  if (navigationState.routes[navigationState.index].key === scene.key) // New scene is current scene
+    return navigationState;
+  const index = navigationState.routes.findIndex((route) => {
+    return scene.key === route.key
+  });
+  if (index > -1) { // New scene is already in navigationState;
+    const clonedState = Object.assign({}, navigationState);
+    clonedState.routes.splice(index, 1);
+    return NavigationStateUtils.push(clonedState, scene);
   }
-};
+  return NavigationStateUtils.push(navigationState, scene);
+}
 
 function changeNavigationState(navigationState, type: 'push' | 'pop', scene?) {
   switch (type) {
     case 'push':
       if(!scene) return navigationState;
-      if (navigationState.routes[navigationState.index].key === scene.key)
-        return navigationState;
-      const index = navigationState.routes.findIndex((route) => {
-        return scene.key === route.key
-      });
-      if (index > -1) {
-        const clonedState = Object.assign({}, navigationState);
-        clonedState.routes.splice(index, 1);
-        return NavigationStateUtils.push(clonedState, scene);
-      }
-      return NavigationStateUtils.push(navigationState, scene);
+      return pushScene(navigationState, scene);
     case 'pop':
       return NavigationStateUtils.pop(navigationState);
   }
   return navigationState;
 }
 
-export default (state = initialModel, action) => {
+interface IState{
+  navigationState: {
+    index: number,
+    routes: IScene[]
+  }
+}
+
+const initialModel = {
+  navigationState: {
+    index: 0,
+    routes: [scenes.vacancies()]
+  }
+};
+
+export default (state: IState = initialModel, action) => {
   switch (action.type) {
     case PUSH_SCENE:
       return Object.assign({}, state, {
