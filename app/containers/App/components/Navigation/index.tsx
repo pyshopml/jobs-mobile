2057d462b5740e18e560a354b85e7a1e5fea088c
Danil Migachev <dowhileman@gmail.com>
2017-03-10 02:50:40 +0300

    jobs-055: add Navigation component

---
+++ app/containers/App/components/Navigation/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import autobind from 'autobind-decorator'
+import {View, Text, Image} from 'react-native';
+import {Avatar, Drawer, Divider, COLOR, TYPO} from 'react-native-material-design';
+
+import IScene from 'types/scene.interface';
+import scenes from 'scenes';
+import style from './style';
+
+interface Props {
+  routeKey: string;
+  pushScene(scene:IScene);
+  popScene();
+};
+
+@autobind
+class Navigation extends React.Component<Props, null> {
+  vacanciesSectionItems = () => ([
+    {
+      icon: 'view-list',
+      value: 'Список',
+      onPress: () => this.props.pushScene(scenes.vacancies()),
+      active: this.props.routeKey === 'vacancies',
+    },
+    {
+      icon: 'add-box',
+      value: 'Создать',
+    }
+  ])
+
+  render() {
+    return (
+      <Drawer theme='light'>
+        <Drawer.Header>
+          <View style={style.header}>
+            <Text style={[style.text, COLOR.paperGrey50, TYPO.paperFontSubhead]}>
+              JobsTeamMobile
+            </Text>
+          </View>
+        </Drawer.Header>
+        <Drawer.Section
+          title="Вакансии"
+          items={this.vacanciesSectionItems()}
+        />
+        <Divider style={{ marginTop: 8 }}/>
+      </Drawer>
+    );
+  }
+}
+
+export default Navigation
